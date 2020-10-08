041b545f4c36ced6d19fb3c52aeb3633e1a05c17
Mahelajay <57103078+Mahelajayasinghe@users.noreply.github.com>
2020-10-09 00:32:18 +0530

    login folder

---
+++ Login/forgot_pass.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Recover password</title>
+	<link rel="stylesheet" type="text/css" href="../css/css.css">
+	<style type="text/css">
+		div{border: 1px solid black; padding: 10px 10px;border-radius: 10px; background-color: white; width: 30% ; margin-top: 20%}
+	</style>
+</head>
+<body>
+<center><div align="center">
+	Enter email address<br><br>
+	<form action="">
+		<input type="text" name="email" placeholder="abc@mail.com"><br><br>
+		<input type="submit" name="" value="submit">
+	</form>
+</div></center>
+</body>
+</html>
