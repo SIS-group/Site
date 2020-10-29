71847ac93b127de0d49e2f2f1792ff3a263c965e
Harishchandra98 <harishchandrads98@gmail.com>
2020-10-29 12:32:30 +0530

    updated

---
+++ user_page/assistant_registrar.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Assistant Registrar</title>
+        <link rel="stylesheet" type="text/css" href="../css/sidepanel.css">
+        <!-- <link rel="stylesheet" type="text/css" href="../css/styles.css"> -->
+        <style type="text/css">
+		.sidebar a {
+  			display: block;
+  			color: black;
+  			padding: 15%;
+  			text-decoration: none;
+  			text-align: center;
+		}
+	    </style>
+    </head>
+
+    <body>
+	<div class="sidebar">
+  		<a href="./assistant_registrar/studentpaymentdetails.php">Student Payment Details</a>
+  		<a href=" ./assistant_registrar/studentpersonaldetails.php ">Student's Personal Details</a>
+  		<a href=" studentresultsandgrades.php">Student Results and Grades</a>
+  		<a href=" ">Account settings</a>
+  		<a href="../login/logout.php" style="all:unset ;padding: 25%; "><button>Log out</button></a>
+	</div>
+
+	<div class="content">
+
+  		
+	</div>
+
+    <h2><a href = "../login/logout.php">Sign Out</a></h2>
+</body>
+</html>
